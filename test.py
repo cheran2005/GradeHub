67fae50e5185332c35cf3e8224c64477b17a0eee
varenr <varen.rajoo06@gmail.com>
2025-10-30 23:28:18 -0400

    updated to OCR working

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-print("hello world cheran")
-print("Varen is handsome")
-x = 3
-y=29023
+import pytesseract
+from PIL import Image 
+
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+img = Image.open(r"elecourseoutline.png")
+
+
+text = pytesseract.image_to_string(img)
+
+print("Extracted Text:")
+print(text)
